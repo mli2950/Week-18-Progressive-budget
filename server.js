d39866a59583ea3a0c5d726bd1c9498ea38c4386
Michael Li <Mli2950@gmail.com>
2021-02-12 15:15:27 -0700

    3rd deploy

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(logger("dev"));
